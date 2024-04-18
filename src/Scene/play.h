dba5926de04e59b84eddc826eac483881e93e9f9
ankohina <144640312+ankohina@users.noreply.github.com>
2024-04-18 16:44:38 +0900

    updata
    updata4月18日

--- src/Scene/play.h
+++ src/Scene/play.h
@@ -16,11 +16,21 @@ const char PLAY_IMAGE_PATH[PLAY_IMEG_MAX][255] =
 	"data/play/back.png",			//タイトル背景画像
 };
 
+#define INVINCIBLE_BAR		"data/play/bar.png"		//無敵ゲージバー
+#define INVINCIBLE_GAUGE	"data/play/ゲージ.png"	//無敵ゲージ
+
 class ScenePlay
 {
 private:
 	int imgHandl[PLAY_IMEG_MAX];	//画像ハンドル
 	int HitCount;					//フレームカウント
+
+	int Invincible_Bar_Hndl;				//無敵ゲージバー画像ハンドル
+	int Invincible_Gauge_Hndl;				//無敵ゲージ画像ハンドル
+
+	bool Invincible;				//無敵フラグ
+	int Invincible_Time;			//無敵時間変数
+	int Fox_Count;					//きつねを倒した時のカウント
 	
 public:
 	ScenePlay();
